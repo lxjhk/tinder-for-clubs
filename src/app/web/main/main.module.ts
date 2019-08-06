058dd036a1842561ba16726426efbb51e87b4ae6
dupeng <1961455515@qq.com>
2019-08-06 15:23:44 +0800

    frontend. submit

---
+++ src/app/web/main/main.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main.component';
+const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    }
+];
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+    ],
+    declarations: [MainComponent]
+})
+export class MainModule { }
